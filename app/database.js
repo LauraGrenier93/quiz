4b65a8bd3fb986d1bfe4feabae728cbb2899c0cf
Laura GRENIER <lauragrenier01@gmail.com>
2021-06-21 21:38:13 +0200

    1st attempt to access the heroku db

--- app/database.js
+++ app/database.js
@@ -3,7 +3,7 @@ const {
   Sequelize,
 } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(DATABASE_URL, {
   define: {
     timestamps: false,
   },
